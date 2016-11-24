e8d4ac98a5d566ee76318ecf67bb83a5367cb759
GagarinAP <gagarin.web@gmail.com>
2016-11-24 16:51:18 +0200

    add registr

---
+++ registr.php
@@ -0,0 +1,5 @@
+<?php    
+    print ucfirst("with tabs: <pre>tabbed</pre>");
+    print ucwords("sith spaces: <pre>spaces</pre>");
+    print strtoupper("im not yelling!");
+    print strtolower('<A HREF="one.php">one</A>');
